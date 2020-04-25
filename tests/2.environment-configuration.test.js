1a7039aa23dee41d0e9c61cbd177c728e0767fc5
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-04-25 14:36:35 +0200

    uodating test cases

---
+++ tests/2.environment-configuration.test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/* global describe, it */
+const expect = require('chai').expect
+const request = require('supertest')
+
+describe('Smoke - Environment based configuration', () => {
+  let server
+
+  it('should successfully start server - env based config', async () => {
+    process.env.PORT = 5000
+    process.env.CACHE_ENABLED = false
+
+    const { server: service, port } = require('../server')()
+
+    server = await service.start(port)
+  })
+
+  it('should GET index.html', async () => {
+    await request(server)
+      .get('/')
+      .expect(200)
+      .then((res) => {
+        expect(res.text).to.equal('<h1>Hello from restana-static!</h1>')
+        expect(res.headers['cache-control']).to.equal('public, max-age=0')
+      })
+  })
+
+  it('should successfully terminate the server', async () => {
+    await server.close()
+  })
+})
